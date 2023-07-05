becc96d7f747a95f68f22d8939a139da1acb5069
chunta-chen <chunta.chen@lookout.com>
2023-07-05 18:26:56 +0800

    Fetch remote data. Handle error response
    Implement if-else, for-each template

--- app/src/main/java/gradlelab/DemoController.java
+++ app/src/main/java/gradlelab/DemoController.java
@@ -1,5 +1,8 @@
 package gradlelab;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,10 +11,69 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.ui.Model;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 @RestController
 public class DemoController {
+
+    @GetMapping("/remote_records")
+    public ModelAndView remoteRecords() {
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("records");
+
+        String apiUrl = "https://6242f044d126926d0c59a15f.mockapi.io/userprofile";
+        try {
+            URL url = new URL(apiUrl);
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("GET");
+
+            int responseCode = connection.getResponseCode();
+
+            if (responseCode == HttpURLConnection.HTTP_OK) {
+                BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+                StringBuilder response = new StringBuilder();
+                String line;
+
+                while ((line = reader.readLine()) != null) {
+                    response.append(line);
+                }
+                reader.close();
+                connection.disconnect();
+
+                // Convert response string to JSON objects
+                ObjectMapper objectMapper = new ObjectMapper();
+                List<Record> records = objectMapper.readValue(response.toString(), new TypeReference<List<Record>>() {});
+                System.out.println(records);
+
+                modelAndView.addObject("records", records);
+
+                return modelAndView;
+            } else {
+                // Handle error response
+                try (BufferedReader errorReader = new BufferedReader(new InputStreamReader(connection.getErrorStream()))) {
+                    StringBuilder errorResponse = new StringBuilder();
+                    String errorLine;
+
+                    while ((errorLine = errorReader.readLine()) != null) {
+                        errorResponse.append(errorLine);
+                    }
+
+                    modelAndView.addObject("errorMessage", errorResponse.toString());
+                }
+            }
+        } catch (Exception e) {
+            // Handle exception
+            System.out.println(e.getLocalizedMessage());
+        }
+
+        return modelAndView;
+    }
 
     @GetMapping("/records")
     public ModelAndView helloWorld() {
@@ -19,6 +81,7 @@ public class DemoController {
         modelAndView.setViewName("records");
 
         ArrayList<Record> records = new ArrayList<>();
+
         records.add(new Record("John Doe", 25, "john.doe@example.com"));
         records.add(new Record("Jane Smith", 30, "jane.smith@example.com"));
         records.add(new Record("Bob Johnson", 40, "bob.johnson@example.com"));
--- app/src/main/java/gradlelab/Record.java
+++ app/src/main/java/gradlelab/Record.java
@@ -1,13 +1,69 @@
 package gradlelab;
 
-public class Record {
-    public String name;
-    public int age;
-    public String email;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
-    public Record(String _name, int _age, String _email) {
-        name = _name;
-        age = _age;
-        email = _email;
+public class Record {
+    private String name;
+    private int age;
+    private String email;
+
+    @JsonIgnore
+    private String id = "default";
+
+    public Record() {
+        // Default constructor
+    }
+
+    public Record(String name, int age, String email) {
+        this.name = name;
+        this.age = age;
+        this.email = email;
+    }
+
+    // Getters and Setters
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    // toString() method
+
+    @Override
+    public String toString() {
+        return "Record{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                ", email='" + email + '\'' +
+                ", id='" + id + '\'' +
+                '}';
     }
 }
